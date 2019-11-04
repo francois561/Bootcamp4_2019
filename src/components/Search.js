8e4ecff4588bacd85e3996d6810a9c7c93c74793
francois561 <k.francois@ufl.edu>
2019-11-03 20:35:54 -0500

    done

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,8 @@ import React from 'react';
 class Search extends React.Component {
 	filterUpdate() {
 		//Here you will need to update the value of the filter with the value from the textbox
+		const val = this.myValue.value
+		this.props.filterUpdate(val)
 	}
 	render() {
 		//You will need to save the value from the textbox and update it as it changes
@@ -10,8 +12,13 @@ class Search extends React.Component {
 		
 		return (
 			<form>
-				<input type="text" placeholder="Type to Filter" />
-			</form>
+                    <input
+                    type="text"
+                    ref={ (value) => {this.myValue = value} }
+                    placeholder="Type to filter"
+                    onChange={this.filterUpdate.bind(this)}
+                    />
+                </form>
 		);
 	}
 }
